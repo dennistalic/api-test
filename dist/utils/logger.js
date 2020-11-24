4bfd7f25f285a6709ce7ae950996d980061b5f3a
Dennis Talic <dennistalic@192-168-1-9.tpgi.com.au>
2020-11-25 08:45:22 +1100

    feat: add gitignore

--- dist/utils/logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-function log(request) {
-    console.log("Request type: " + request.method + " Endpoint: " + request.url);
-}
-exports.log = log;
